255a536377ab4bc9a867117f0f6323962cffeda6
hexagonal <hexagonal@example.org>
2015-10-04 12:29:13 +1100

    Tabs to spaces.

--- src/actions/AppActions.ts
+++ src/actions/AppActions.ts
@@ -3,75 +3,75 @@ import AppDispatcher from "../dispatcher/AppDispatcher";
 
 export class GuessLetterAction implements AppAction
 {
-	constructor(public letter: string) {}
+  constructor(public letter: string) {}
 }
 
 export class NewGameAction implements AppAction
 {
-	constructor(public word: string) {}
+  constructor(public word: string) {}
 }
 
 class ActionCreator
 {
-	newGame() {
-		let wordsJson: string = localStorage.getItem('words');
-		let words: string [] = wordsJson ?	JSON.parse(wordsJson) : [];
+  newGame() {
+    let wordsJson: string = localStorage.getItem('words');
+    let words: string [] = wordsJson ?  JSON.parse(wordsJson) : [];
 
-		if (words.length !== 0){
-			console.log('Local storage found.');
-		} else {
-			console.log('Local storage not found. Downloading puzzle file...');
+    if (words.length !== 0){
+      console.log('Local storage found.');
+    } else {
+      console.log('Local storage not found. Downloading puzzle file...');
 
-			// Download word list and then retry
-			let xhr = new XMLHttpRequest();
-			xhr.open('GET', encodeURI('words.txt'));
+      // Download word list and then retry
+      let xhr = new XMLHttpRequest();
+      xhr.open('GET', encodeURI('words.txt'));
 
-			xhr.onload = () => {
-				if (xhr.status === 200) {
-					let unshuffledWords: string[] = xhr.responseText.split(/\r?\n/);
-					let words: string[] = shuffle(unshuffledWords);
-					writeLocalWords(words);
-					this.newGame();
-				} else {
-					console.log('Puzzle file not found.')
-				}
-			};
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          let unshuffledWords: string[] = xhr.responseText.split(/\r?\n/);
+          let words: string[] = shuffle(unshuffledWords);
+          writeLocalWords(words);
+          this.newGame();
+        } else {
+          console.log('Puzzle file not found.')
+        }
+      };
 
-			xhr.send();
-			return;
-		}
+      xhr.send();
+      return;
+    }
 
-		let word: string = words.pop();
-		writeLocalWords(words);
+    let word: string = words.pop();
+    writeLocalWords(words);
 
-		AppDispatcher.dispatch(new NewGameAction(word));
-	}
+    AppDispatcher.dispatch(new NewGameAction(word));
+  }
 
-	guessLetter(letter: string) {
-		AppDispatcher.dispatch(new GuessLetterAction(letter));
-	}
+  guessLetter(letter: string) {
+    AppDispatcher.dispatch(new GuessLetterAction(letter));
+  }
 } // end ActionCreator
 
 function loadLocalWords(): string[] {
-	let wordsJson: string = localStorage.getItem('words');
-	return wordsJson ? JSON.parse(wordsJson) : [];
+  let wordsJson: string = localStorage.getItem('words');
+  return wordsJson ? JSON.parse(wordsJson) : [];
 }
 
 function writeLocalWords(words: string[]) {
-	let wordsJson: string = JSON.stringify(words);
-	localStorage.setItem('words', wordsJson);
+  let wordsJson: string = JSON.stringify(words);
+  localStorage.setItem('words', wordsJson);
 }
 
 // Shuffles an array
 // Implementation taken from: stackoverflow.com/questions/962802
 function shuffle<T>(arr: T[]): T[] {
-		let top: number = arr.length;
+    let top: number = arr.length;
 
     if(top) while(--top) {
-    	let current: number = Math.floor(Math.random() * (top + 1));
-    	let item: T = arr[current];
-    	arr[current] = arr[top];
-    	arr[top] = item;
+      let current: number = Math.floor(Math.random() * (top + 1));
+      let item: T = arr[current];
+      arr[current] = arr[top];
+      arr[top] = item;
     }
 
     return arr;
@@ -79,7 +79,7 @@ function shuffle<T>(arr: T[]): T[] {
 
 // Returns a random item from an array
 function getRandomItem<T>(arr: T[]): T {
-	return arr[getRandomInt(0, arr.length)];
+  return arr[getRandomInt(0, arr.length)];
 }
 
 // Returns a random integer between min (included) and max (excluded)
--- src/stores/AppStore.ts
+++ src/stores/AppStore.ts
@@ -9,101 +9,101 @@ import {GuessLetterAction, NewGameAction} from "../actions/AppActions";
 
 export interface AppState
 {
-		word: string;
-		correctGuesses: Immutable.Set<string>;
-		incorrectGuesses: Immutable.Set<string>;
+    word: string;
+    correctGuesses: Immutable.Set<string>;
+    incorrectGuesses: Immutable.Set<string>;
 }
 
 export class AppStateHelp
 {
-	constructor(private state: AppState) {}
+  constructor(private state: AppState) {}
 
-	isCorrectGuess(letter: string): boolean {
-		return this.state.correctGuesses.some(x => x === letter)
-	}
+  isCorrectGuess(letter: string): boolean {
+    return this.state.correctGuesses.some(x => x === letter)
+  }
 
-	isIncorrectGuess(letter: string): boolean {
-		return this.state.incorrectGuesses.some(x => x === letter)
-	}
+  isIncorrectGuess(letter: string): boolean {
+    return this.state.incorrectGuesses.some(x => x === letter)
+  }
 
-	get puzzle(): string[] {
-		let letters: string[] = this.state.word.split('');
+  get puzzle(): string[] {
+    let letters: string[] = this.state.word.split('');
 
-		let puzzle: string[] =
-			letters.map(letter =>
-				this.isCorrectGuess(letter) ? letter : '_'
-			);
+    let puzzle: string[] =
+      letters.map(letter =>
+        this.isCorrectGuess(letter) ? letter : '_'
+      );
 
-		return puzzle;
-	}
+    return puzzle;
+  }
 }
 
 class AppStore
 {
   private _ee: EventEmitter = new EventEmitter;
 
-	private _state: AppState = {
-		word: '',
-		correctGuesses: Immutable.Set<string>(),
-		incorrectGuesses: Immutable.Set<string>()
-	};
+  private _state: AppState = {
+    word: '',
+    correctGuesses: Immutable.Set<string>(),
+    incorrectGuesses: Immutable.Set<string>()
+  };
 
   get state(): AppState { return this._state; }
 
   constructor() {
     AppDispatcher.register((action: AppAction) => {
 
-			if (action instanceof NewGameAction) {
-				this.newGame(action.word);
-			} else if (action instanceof GuessLetterAction) {
-				this.guessLetter(action.letter);
+      if (action instanceof NewGameAction) {
+        this.newGame(action.word);
+      } else if (action instanceof GuessLetterAction) {
+        this.guessLetter(action.letter);
       }
     });
   }
 
-	newGame(word: string)
-	{
-		this._state = {
-			word: word,
-			correctGuesses: Immutable.Set<string>(),
-			incorrectGuesses: Immutable.Set<string>()
-		}
+  newGame(word: string)
+  {
+    this._state = {
+      word: word,
+      correctGuesses: Immutable.Set<string>(),
+      incorrectGuesses: Immutable.Set<string>()
+    }
 
-		this.raiseChangeEvent();
-	}
+    this.raiseChangeEvent();
+  }
 
-	guessLetter(letter: string){
-		let wordLetters: string[] = this.state.word.split('');
-		let correctGuesses = this.state.correctGuesses;
-		let incorrectGuesses = this.state.incorrectGuesses;
+  guessLetter(letter: string){
+    let wordLetters: string[] = this.state.word.split('');
+    let correctGuesses = this.state.correctGuesses;
+    let incorrectGuesses = this.state.incorrectGuesses;
 
-		let isCorrectGuess: boolean = wordLetters.some((x) => x === letter);
+    let isCorrectGuess: boolean = wordLetters.some((x) => x === letter);
 
-		if(isCorrectGuess) {
-			this.state.correctGuesses = correctGuesses.add(letter);
-		} else {
-			this.state.incorrectGuesses = incorrectGuesses.add(letter);
-		}
+    if(isCorrectGuess) {
+      this.state.correctGuesses = correctGuesses.add(letter);
+    } else {
+      this.state.incorrectGuesses = incorrectGuesses.add(letter);
+    }
 
-		let isChanged: boolean =
-			this.state.correctGuesses !== correctGuesses ||
-			this.state.incorrectGuesses !== incorrectGuesses;
+    let isChanged: boolean =
+      this.state.correctGuesses !== correctGuesses ||
+      this.state.incorrectGuesses !== incorrectGuesses;
 
-		if (isChanged) { this.raiseChangeEvent(); }
-	}
+    if (isChanged) { this.raiseChangeEvent(); }
+  }
 
-	raiseChangeEvent()
-	{
-		this._ee.emit("change");
-	}
+  raiseChangeEvent()
+  {
+    this._ee.emit("change");
+  }
 
-	addChangeListener(callback: Function) {
-		this._ee.on("change", callback);
-	}
+  addChangeListener(callback: Function) {
+    this._ee.on("change", callback);
+  }
 
-	removeChangeListener(callback: Function) {
-		this._ee.removeListener("change", callback);
-	}
+  removeChangeListener(callback: Function) {
+    this._ee.removeListener("change", callback);
+  }
 }
 
 export default new AppStore();
